58e4383f5f32f7721eb4ca98e563e2bedbb6bbf8
lampochka-ilicha <lampochka_ilicha@mail.ru>
2015-02-13 16:07:07 +0300

    Quaternin logic

---
+++ src/main/java/com/avisha_neu/Manipulation.java
@@ -0,0 +1,10 @@
+package com.avisha_neu;
+
+/**
+ * Created by tatyana.kalnitskaya on 13.02.15.
+ */
+public class Manipulation {
+    public static void rotate(){
+
+    }
+}
---
+++ src/main/java/com/avisha_neu/math/Vector.java
@@ -0,0 +1,74 @@
+package com.avisha_neu.math;
+
+/**
+ * Created by tatyana.kalnitskaya on 13.02.15.
+ */
+public class Vector {
+    private double x;
+    private double y;
+    private double z;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public Vector(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getNorm() {
+        return x*x + y*y + z*z;
+    }
+
+    public Vector getConjugated() {
+        return new Vector (-x, -y, -z);
+    }
+
+    public Vector multiply(double multiplier) {
+        return new Vector(x*multiplier,
+                y*multiplier,
+                z*multiplier);
+    }
+
+    public Vector multiply(Vector multiplier) {
+        return new Vector(y*multiplier.z - z*multiplier.y,
+                z*multiplier.x - x*multiplier.z,
+                x*multiplier.y - y*multiplier.x);
+    }
+
+    public double multiplyScalar(Vector multiplier) {
+        return x*multiplier.x +
+                y*multiplier.y +
+                z*multiplier.z;
+    }
+
+    public Vector add(Vector summand) {
+        return new Vector(x + summand.x,
+                y + summand.y,
+                z + summand.z);
+    }
+
+
+}
