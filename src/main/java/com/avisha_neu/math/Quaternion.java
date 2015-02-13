941832ce87591ec2de827bfb7373b796f57d3863
lampochka-ilicha <lampochka_ilicha@mail.ru>
2015-02-13 16:05:45 +0300

    Quaternin logic

--- src/main/java/com/avisha_neu/math/Quaternion.java
+++ src/main/java/com/avisha_neu/math/Quaternion.java
@@ -4,5 +4,87 @@ package com.avisha_neu.math;
  * Created by Tatyana.Kalniskaya on 11.02.15.
  */
 public class Quaternion {
-    boolean fool = false;
+    private double phi;
+    private Vector v;
+
+    public Quaternion(double x, double y, double z, double phi) {
+        this.v.setX(x);
+        this.v.setY(y);
+        this.v.setZ(z);
+        this.phi = phi;
+    }
+
+    public Quaternion(Vector v, double phi) {
+        this.v = v;
+        this.phi = phi;
+    }
+
+    public double getPhi() {
+        return phi;
+    }
+
+    public void setPhi(double phi) {
+        this.phi = phi;
+    }
+
+    public Vector getV() {
+        return v;
+    }
+
+    public void setV(Vector v) {
+        this.v = v;
+    }
+
+
+    public double getX() {
+        return v.getX();
+    }
+
+    public void setX(double x) {
+        this.v.setX(x);
+    }
+
+
+    public double getY() {
+        return v.getY();
+    }
+
+    public void setY(double y) {
+        this.v.setY(y);
+    }
+
+    public double getZ() {
+        return v.getZ();
+    }
+
+    public void setZ(double z) {
+        this.v.setZ(z);
+    }
+
+    public Quaternion multiply(double multiplier) {
+        return new Quaternion(v.multiply(multiplier),
+                phi*multiplier);
+    }
+
+    public Quaternion multiply(Quaternion multiplier) {
+        return new Quaternion(
+                v.multiply(multiplier.v)
+                        .add(multiplier.v.multiply(phi)
+                        .add(v.multiply(multiplier.phi))),
+                phi*multiplier.phi - v.multiplyScalar(multiplier.v));
+    }
+
+    public double getNorm(){
+        return v.getNorm() + phi*phi;
+    }
+
+    public  Quaternion getConjugated() {
+        return new Quaternion(v.getConjugated(), phi);
+    }
+
+    public  Quaternion getInverse() {
+        return   getConjugated().multiply(1.0/getNorm());
+    }
+
+
 }
