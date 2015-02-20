881ab131ab1127fd3f79515398bff0e921e03bf4
lampochka-ilicha <lampochka_ilicha@mail.ru>
2015-02-20 15:59:59 +0300

    Quaternion constructors

--- src/main/java/com/avisha_neu/math/Manipulator.java
+++ src/main/java/com/avisha_neu/math/Manipulator.java
@@ -3,11 +3,9 @@ package com.avisha_neu.math;
 public class Manipulator {
 
     public static Vector rotateVector(Vector axis, double angle, Vector vector){
-        Quaternion q = new Quaternion(axis, angle, true);
+        Quaternion q = Quaternion.rawCreate(axis, angle);
         Quaternion q1 = q.getInverse();
         return q.multiply(Quaternion.fromVector(vector)).multiply(q1).toVector();
-
-
 
     }
 }
